6f9be190cb8312a82510923df5447a92fee1cc7d
Eze Ochonogor <129423073+anymail4eze@users.noreply.github.com>
2023-04-04 12:32:51 +0600

    Add files via upload

---
+++ src/modules/Home/HomePage.jsx
@@ -0,0 +1,23 @@
+import Faq from "./Features/Faq/Faq";
+import Features from "./Features/Features";
+import GettingStart from "./Features/GettingStart";
+import Hero from "./Features/Hero";
+import Testimonial from "./Features/Testimonial/Testimonial";
+import WhatDo from "./Features/WhatDo";
+
+const Home = () => {
+  return (
+    <>
+      <div className="bg-[#EDF9FF] lg:h-[987px] md:h-[843px] h-[1373px] ">
+        <Hero />
+      </div>
+      <WhatDo />
+      <Features />
+      <Testimonial />
+      <Faq />
+      <GettingStart />
+    </>
+  );
+};
+
+export default Home;
